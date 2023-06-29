d0118f6beae4f1544d44066dc5760521334c1efe
Mehmet Uyanik <mehmetc.uyanik@gmail.com>
2023-06-29 13:10:49 +0300

    components and services ara added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { ButtonComponent } from './button/button.component';
+import { TableComponent } from './table/table.component';
+import { UserComponent } from './user/user.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PostDetailsComponent,
+    ButtonComponent,
+    TableComponent,
+    UserComponent,
+    UsersComponent,
+    PostsComponent,
+    CategoriesComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
